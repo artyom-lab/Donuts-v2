6143649806ce8f34d103926f215a5d5eb1a8c0c3
artyom-lab <artyomgalkeane@gmail.com>
2023-03-23 13:28:23 +0300

    589

--- js/common.js
+++ js/common.js
@@ -9,55 +9,27 @@ $(function() {
   Waves.attach('.btn',  ['waves-light']);
   Waves.init();
 
-((w, d, u, n) => {
-  let opCSlider = {pause: (ms) => new Promise((r) => setTimeout(r, ms)),
-  events: {
-    click: async (e) => {
-      if (e.target.classList.contains(`arrow-left`) &&
-          e.target.parentElement.parentElement.classList.contains(
-              `module-cascade-slider`)) {
-          e.target.parentElement.parentElement.children[1].children[0].classList.add(
-              `glide-up-first`);
-          await opCSlider.pause(200);
-          e.target.parentElement.parentElement.children[1].children[0].classList.remove(
-              `glide-up-first`);
-          e.target.parentElement.parentElement.children[1].children[0].classList.add(
-              `glide-up-last`);
-          e.target.parentElement.parentElement.children[1].appendChild(
-          e.target.parentElement.parentElement.children[1].children[0]);
-          await opCSlider.pause(200);
-          e.target.parentElement.parentElement.children[1].lastElementChild.classList.remove(
-              `glide-up-last`);}
-      if (e.target.classList.contains(`arrow-right`) &&
-          e.target.parentElement.parentElement.classList.contains(
-              `module-cascade-slider`)) {
-          e.target.parentElement.parentElement.children[1].lastElementChild.classList.add(
-              `glide-down-last`);
-          await opCSlider.pause(200);
-          e.target.parentElement.parentElement.children[1].lastElementChild.classList.remove(
-              `glide-down-last`);
-          e.target.parentElement.parentElement.children[1].lastElementChild.classList.add(
-              `glide-up-first`);
-          e.target.parentElement.parentElement.children[1].insertBefore(
-          e.target.parentElement.parentElement.children[1].lastElementChild,
-          e.target.parentElement.parentElement.children[1].children[0]);
-          await opCSlider.pause(200);
-          e.target.parentElement.parentElement.children[1].children[0].classList.remove(
-              `glide-up-first`);}
-        }
-      }
-    };
-    d.addEventListener(`click`, opCSlider.events.click);
-    w.addEventListener(`beforeunload`, () => {
-    d.removeEventListener(`click`, opCSlider.events.click);
-    opCSlider = u;});
-  })(
-    window,
-    document,
-    undefined,
-    null
-  );
+  $('.owl-1').owlCarousel({
+    center: true,
+    items: 3,
+    loop: true,
+    margin: 0,
+    dots: false,
+    responsive: {
+      0: {
+        mouseDrag : true,
+      },
+      1200: {
+        mouseDrag : false,
+      },
+    },
+  });
 
+  $(document).on('click', '.owl-item', function() {
+  owlIndex = $(this).index();
+  count = document.querySelectorAll(".owl-item.active").length;
+  $('.owl-stage-outer').trigger('to.owl.carousel', owlIndex - count);
+});
 
   var popupTimer;
   function delayPopup(popup) {
